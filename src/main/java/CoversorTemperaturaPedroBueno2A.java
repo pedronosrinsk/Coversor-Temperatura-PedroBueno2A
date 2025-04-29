01143e553b6eecaa80398d9f8ec7bfea1543eb70
Pedro Correa Bueno <pb54791@gmail.com>
2025-04-29 08:41:47 -0400

    Primeiro Commit

---
+++ src/main/java/CoversorTemperaturaPedroBueno2A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author 2024105070023
+ */
+public class CoversorTemperaturaPedroBueno2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
